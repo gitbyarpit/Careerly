42f3a8da179d66177b23cc18b45da11d30011b81
Arpit Agarwal <arpitbnntt2180@gmail.com>
2025-11-28 01:43:52 +0530

    Update dashboard-view.jsx

--- app/(main)/dashboard/_components/dashboard-view.jsx
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -164,7 +164,7 @@ const DashboardView = ({insights}) => {
                           <p className="font-medium">{label}</p>
                           {payload.map((item) => (
                             <p key={item.name} className="text-sm">
-                              {item.name}: ${item.value}K
+                              {item.name}: ₹{item.value}K
                             </p>
                           ))}
                         </div>
