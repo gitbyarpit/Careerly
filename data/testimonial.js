cefbcda477afd5bb58b7b5bea168693e65217f1b
Arpit Agarwal <arpitbnntt2180@gmail.com>
2025-11-28 02:58:52 +0530

    Update testimonial.js

--- data/testimonial.js
+++ data/testimonial.js
@@ -11,7 +11,7 @@ export const testimonial = [
     quote:
       "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
     author: "Manoj Gupta",
-    image: "https://www.freepik.com/free-photo/cheerful-indian-businessman-smiling-closeup-portrait-jobs-career-campaign_17830501.htm#fromView=search&page=1&position=4&uuid=a99adc65-a2c8-45f4-9e8b-47f995c19087&query=Indian+man",
+    image: "https://randomuser.me/api/portraits/men/7.jpg",
     role: "Product Manager",
     company: "StartUp Inc.",
   },
@@ -27,7 +27,7 @@ export const testimonial = [
     quote:
       "I loved how tailored the feedback was — it felt like having a personal mentor guiding me through every step.",
     author: "Mukesh Yadav",
-    image: "https://www.freepik.com/free-photo/closeup-young-hispanic-man-casuals-studio_27507633.htm#fromView=search&page=1&position=0&uuid=a99adc65-a2c8-45f4-9e8b-47f995c19087&query=Indian+man",
+    image: "https://randomuser.me/api/portraits/men/89.jpg",
     role: "Data Analyst",
     company: "Insight Labs",
   },
@@ -35,7 +35,7 @@ export const testimonial = [
     quote:
       "The AI suggestions helped me refine my portfolio perfectly. Recruiters started reaching out almost instantly!",
     author: "Aisha Rahman",
-    image: "https://www.freepik.com/free-photo/pleased-young-brunette-caucasian-woman-stands-sideways-with-crossed-arms_16923342.htm#fromView=search&page=1&position=37&uuid=367aa53c-17ca-467e-a66f-e781c0b30521&query=Indian+man",
+    image: "https://randomuser.me/api/portraits/women/71.jpg",
     role: "UI/UX Designer",
     company: "Creative Pulse",
   },
@@ -43,7 +43,7 @@ export const testimonial = [
     quote:
       "From resume optimization to mock interviews, everything felt seamless. Definitely the smartest prep tool out there!",
     author: "Yash Sharma",
-    image: "https://www.freepik.com/premium-photo/portrait-young-handsome-indian-man-against-gray-background_9495076.htm#fromView=search&page=1&position=8&uuid=bb8ae3e0-b3a2-46d0-a238-d7174911dfad&query=Indian+man",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
     role: "Software Developer",
     company: "NextGen Solutions",
   },
